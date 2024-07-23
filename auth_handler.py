f8731f938b0e93a6aba000cf33d273511a346959
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2024-07-23 14:46:09 -0500

    testci

--- auth_handler.py
+++ auth_handler.py
@@ -6,7 +6,6 @@ from jose import jwt
 from config import SECRET_ACCESS_KEY, ALGORITHM
 
 
-
 class AuthHandler(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         if request.method == "OPTIONS":
